1823f3fa37964eda48d28597612f597a80a551a6
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2025-07-24 14:24:24 +1200

    Time: 2 ms (41.20%) | Memory: 58.1 MB (45.82%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -3,35 +3,25 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    //fizz buzz when both divisbal by 3 and 5
 
-    //fizz when devisible by 3
+    let result = [];
 
-    //buzz when divisibal by 5
-
-    let arr = [];
-
-    //go all the way up for the number of times
-    for(let i = 1; i < n + 1; i ++){
-        if(i % 5 == 0 && i % 3 == 0){
-            //check if it is divisible by both so add FizzBuzz
-            arr.push("FizzBuzz");
+    for(let i = 1; i <= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            result.push("FizzBuzz");
         }
-        else if(i % 5 == 0){
-            //check if its divisable by 5 so add Buzz
-            arr.push("Buzz");
+        else if(i % 3 === 0){
+            result.push("Fizz");
         }
-        else if(i % 3 == 0){
-            //check if its divisible by 3 so add Fizz
-            arr.push("Fizz");
+        else if(i % 5 === 0){
+            result.push("Buzz");
         }
         else{
-            //isn't divisibal by either of them so just add the number
-            arr.push(String(i));
+            result.push(String(i));
         }
+
     }
 
-    return arr;
-
+    return result;
     
 };
