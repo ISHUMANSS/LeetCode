399ebd8a22d6e3e8b0b6b645a675e34237a921b6
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2025-08-02 14:14:05 +1200

    Time: 1 ms (86.27%) | Memory: 56.1 MB (97.44%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    //if it is smaller go to the left if it is larger go to the right
+    //need to find the root and then we can go from there
+    //we want the root to be like the middle or something
+    //use recursion to be able to loop through all the numbers
+    function btree(left, right){
+        //node is fine
+        if(left > right){
+            return null;
+        }
+
+        //get mid
+        const mid = Math.floor((left + right) / 2);
+        const node = new TreeNode(nums[mid]);
+
+        //balance the tree recursively
+        node.left = btree(left, mid - 1);
+        node.right = btree(mid +1, right);
+
+        return node;
+    }
+
+    return btree(0, nums.length -1);
+
+
+    
+};
