88b9dd325caf2f5eaadf2c947306341d831fbe4d
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2025-11-26 13:46:12 +1300

    Time: 2 ms (67.31%) | Memory: 42.8 MB (15.06%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        //find if there is a subsequence of t inside the 
+        //if the characters show up in order then it is true
+
+        //need to keep track of the characters we have seen if they match the wanted subsequence
+
+        //if our points for the two string match each other then we can move both of them to the next position
+        //when we get to the end of s we can stop
+
+        int s_pointer = 0;
+        int t_pointer = 0;
+
+        //loop through if the pointers are still in range
+        while(s_pointer < s.length() && t_pointer < t.length()){
+            if(s.charAt(s_pointer) == t.charAt(t_pointer)){
+                s_pointer ++;
+            }
+            t_pointer ++;
+        }
+        return s_pointer == s.length();
+    }
+}
