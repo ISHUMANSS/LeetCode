2c9f2335f7edf0fa83eea325ba013ecc88f921f1
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2025-11-27 13:25:03 +1300

    Time: 2 ms (67.34%) | Memory: 42.6 MB (31.18%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        //find if there is a subsequence of t inside the 
-        //if the characters show up in order then it is true
+        //keep track of a pointer to both of the arrays
+        //increase both of them when the answer is seen 
+        //increase t when it doesn't match
 
-        //need to keep track of the characters we have seen if they match the wanted subsequence
+        int spoint = 0;
+        int tpoint = 0;
 
-        //if our points for the two string match each other then we can move both of them to the next position
-        //when we get to the end of s we can stop
-
-        if (s.length() == 0) return true;
-        if (s.equals(t)) return true;
-
-        int s_pointer = 0;
-        int t_pointer = 0;
-
-        //loop through if the pointers are still in range
-        while(s_pointer < s.length() && t_pointer < t.length()){
-            if(s.charAt(s_pointer) == t.charAt(t_pointer)){
-                s_pointer ++;
+        while(spoint < s.length() && tpoint < t.length()){
+            if(s.charAt(spoint) == t.charAt(tpoint)){
+                spoint ++;
             }
-            t_pointer ++;
+            tpoint ++;
         }
-        return s_pointer == s.length();
+
+
+        return spoint == s.length();
+
+
     }
 }
