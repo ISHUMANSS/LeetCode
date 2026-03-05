9386c39d262d9f7f1c38f6c71bb55ede15d1d7e9
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2026-03-05 19:34:56 +1300

    Time: 0 ms (100.00%) | Memory: 48.3 MB (28.21%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+
+
+
+        int[] arr = {-1, -1};
+
+        //quick exit array is to small
+        if(arr.length == 0){
+            return arr;
+        }
+
+
+        //run binary search and then use two pointers to find the outer edeges
+        int res = binarySearch(nums, 0, nums.length - 1, target);
+
+
+
+        //not found
+        if(res == -1){
+            return arr;
+        }
+
+        //use two pointer from the index to be able to find the outer bounds
+        int left = res;
+        int right = res;
+        
+        //loop through to find the outer bounds of the target number
+        //expand left
+        while (left - 1 >= 0 && nums[left - 1] == target) {
+            left--;
+        }
+
+        //expand right
+        while (right + 1 < nums.length && nums[right + 1] == target) {
+            right++;
+        }
+
+        arr[0] = left;
+        arr[1] = right;
+
+
+        return arr;
+
+
+    }
+
+
+    static int binarySearch(int a[], int l, int r, int x){
+
+      	if (r >= l) {
+            int m = l + (r - l) / 2;
+
+            // Returned Index of the Element
+            if (a[m] == x)
+                return m;
+
+            // If element is smaller than mid, then
+            // it can only be present in left subarray
+            if (a[m] > x)
+                return binarySearch(a, l, m - 1, x);
+
+            // Else the element can only be present
+            // in right subarray
+            return binarySearch(a, m + 1, r, x);
+        }
+
+        // No Element Found
+        return -1;
+    }
+}
