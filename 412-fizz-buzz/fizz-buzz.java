343aa83617448c1b1342fbe90ce6bb4f008cacc9
Is_human <104465511+ISHUMANSS@users.noreply.github.com>
2026-05-07 14:29:13 +1200

    Time: 1 ms (100.00%) | Memory: 46.6 MB (91.85%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
 
-        List<String> words = new ArrayList<>();
-
-        for(int i = 1; i <= n; i++){
-            if(i % 5 == 0 && i % 3 == 0){
-                words.add("FizzBuzz");
-            }
-            else if(i % 5 == 0){
-                words.add("Buzz");
+        for(int i = 1; i < n + 1; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                res.add("FizzBuzz");
             }
             else if(i % 3 == 0){
-                words.add("Fizz");
+                res.add("Fizz");
+            }
+            else if(i % 5 == 0){
+                res.add("Buzz");
             }
             else{
-                words.add(String.valueOf(i));
+                res.add(Integer.toString(i));
             }
         }
 
-        return words;
+        return res;
         
     }
 }
